e63ceb7f377784804e21d2d1c71bdb2aed0dde06
Samir <samir.deraiya@mindinventory.com>
2021-11-11 17:10:23 +0530

    Initial Project Commit

---
+++ FunProject/FunProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunProject
+//
+//  Created by Mac-0006 on 11/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
